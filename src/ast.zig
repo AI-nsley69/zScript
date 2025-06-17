118616d66071f7e57a0ad52609ee863b819b6d17
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2025-06-17 19:35:31 +0200

    Current progress

---
+++ src/ast.zig
@@ -0,0 +1,25 @@
+const runtime = @import("runtime.zig");
+const scanner = @import("scanner.zig");
+
+pub const ExpressionType = enum {
+    expr,
+    literal,
+};
+
+pub const ExpressionValue = union(ExpressionType) {
+    expr: *Expression,
+    literal: runtime.Value,
+};
+
+pub const Operands = enum {
+    Add,
+    Subtract,
+    Divide,
+    Multiply,
+};
+
+pub const Expression = struct {
+    lhs: ExpressionValue,
+    operand: ?Operands,
+    rhs: ?ExpressionValue,
+};
---
+++ src/parser.zig
@@ -0,0 +1,1 @@
+const ast = @import("ast.zig");
