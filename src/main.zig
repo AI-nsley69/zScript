2799c381073ba1a48e0d9d72a1ce0a53a755990c
AI-nsley69 <hello@benji.foo>
2025-07-24 10:51:37 +0200

    Create a function for benchmarking performance

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const lib = @import("lib.zig");
 const cli = @import("cli/root.zig");
+const builtin = @import("builtin");
+
+var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+
+// Function to strip unnecessary overhead when looking at performance of certain parts of the runtime.
+fn benchmark() !void {
+    _ = try lib.run(std.heap.smp_allocator, @embedFile("./bench.zs"), .{});
+}
 
 pub fn main() !void {
     const allocator = std.heap.smp_allocator;
