36ee620017a2f8333fb137102d5d5d68ff1db079
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2025-07-29 20:56:30 +0200

    Add test for float

--- src/test.zig
+++ src/test.zig
@@ -16,3 +16,16 @@ test "Integer Arithmetic" {
     const has_leak = debug_gpa.deinit();
     try expect(has_leak == .ok);
 }
+
+test "Float Arithmetic" {
+    const gpa = debug_gpa.allocator();
+
+    const file = "tests/float_arithmetic.zs";
+    const val = try lib.run(gpa, @embedFile(file), .{ .file = file });
+    try expect(val != null);
+    try expect(val.? == .float);
+    try expect(val.?.float == 1.5);
+    // Test for potential leaks
+    const has_leak = debug_gpa.deinit();
+    try expect(has_leak == .ok);
+}
